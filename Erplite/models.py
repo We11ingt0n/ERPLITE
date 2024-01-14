f0887d1b90f8200b6aeb07aa97a6426af0619597
Wellington Santos <wellington.da.s.stos@gmail.com>
2024-01-14 20:12:30 -0300

    migration

--- Erplite/models.py
+++ Erplite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class login(models.Model):
+
+    login = models.CharField(max_length=15)
+    senha = models.CharField(max_length=15)
+
+class Colaboradores (models.Model):
+    
+    nome = models.CharField(max_length=15)
+    sobreNome = models.CharField(max_length=30)
+    cpf = models.CharField(max_length=12)
+    rg = models.CharField(max_length=12)
+    ctps = models.CharField(max_length=15)
+    pis = models.CharField(max_length=15)
+    endereco = models.CharField(max_length=50)
+    complemento = models.CharField(max_length=15)
+    numero = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    
+    
+    
